b6fd937a9560354f695067b7b1a69d31ca4db58b
Sergio Camargo <sergiochidalgo@gmail.com>
2017-06-05 16:01:41 +0000

    hotfix: get all polls of user

--- app/controllers/pollHandler.server.js
+++ app/controllers/pollHandler.server.js
@@ -7,9 +7,9 @@ var Poll = require('../models/polls.js');
 
 function PollHandler () {
 
-	this.getPolls = function (req, res) { // TODO: debug
+	this.getPolls = function (req, res) {
 		Poll
-			.findOne({ 'ownerOfPoll': req.user._id }, { '_id': false })
+			.find({ 'ownerOfPoll': req.user._id }, { '_id': false })
 			.exec(function (err, result) {
 				if (err) { throw err; }
 
@@ -33,11 +33,7 @@ function PollHandler () {
 				
 	        	poll.save(function(err) {
 		            if (err) { throw err; }
-		            //user.pollsOfUser.push(poll);
-		            //user.save(function(err) {
-		                //if (err) { throw err; }
-		                res.send('Added poll');
-		            //});
+	                res.send('Added poll');
         		});
 				
 			});
