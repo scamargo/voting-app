b7b376d8787c0c094ca8e82ecb42379445503454
Sergio Camargo <sergiochidalgo@gmail.com>
2017-07-07 20:30:03 +0000

    User can add/remove options from when creating poll

--- app/controllers/createPollController.client.js
+++ app/controllers/createPollController.client.js
@@ -6,7 +6,7 @@
 
    var questionInput = document.querySelector("#question");
    var pollUrl = appUrl + '/api/:id/polls';
-   var pollOptions = document.querySelectorAll('.option');
+   var pollOptions;
    var createPollButton = document.querySelector('#create-poll');
    
    createPollButton.addEventListener('click', function () {
@@ -18,12 +18,37 @@
            savePoll();
        }
    });
+   
+   $('#add-option').click(function(){
+      var div = document.createElement('div');
+      var input = document.createElement('input');
+      var button = document.createElement('button');
+      var text = document.createTextNode('-');
+      
+      div.setAttribute('style','display:block');
+      input.setAttribute('class','option');
+      input.setAttribute('style','display:inline');
+      button.onclick = deleteEvent; // Attach the event!
+      button.setAttribute('class','btn btn-delete');
+      button.appendChild(text);
+      
+      div.appendChild(input);
+      div.appendChild(button);
+      $('#options').append(div);
+   });
+   
+   function deleteEvent() {
+      console.log('delete');
+      $(this).parent('div').remove();
+   }
 
    function savePoll() {
       var urlWithQuery = pollUrl + '?question=' + questionInput.value;
-      //TODO: loop through options and add them
-      urlWithQuery += '&options[]=' + pollOptions[0].value;
-      urlWithQuery += '&options[]=' + pollOptions[1].value;
+      pollOptions = document.querySelectorAll('.option');
+      // loop through options and add them to query string
+      for(var i=0;i<pollOptions.length;i++) {
+         urlWithQuery += '&options[]=' + pollOptions[i].value;   
+      }
 
       ajaxFunctions.ready(ajaxFunctions.ajaxRequest('POST', urlWithQuery, function (data) {
          var pollObject = JSON.parse(data);
